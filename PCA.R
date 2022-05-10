15c1d3e926deaab82325a1c5883d1ad2b14fc1f3
yanny <77775253+yan-ww@users.noreply.github.com>
2022-05-10 20:06:39 +0800

    Create PCA.R

---
+++ PCA.R
@@ -0,0 +1,29 @@
+setwd("E:/BIO/EP/data/")
+library(DESeq2)
+library(factoextra)
+library(FactoMineR)
+library(vegan)
+
+DEgenecount<-read.csv("DEgenecount.csv")
+rownames(DEgenecount)<-DEgenecount[,1]
+DEgenecount<-DEgenecount[,-1]
+group<-read.csv("group.CSV")
+condition<-group$GROUP
+coldata<-data.frame(row.names=colnames(DEgenecount),condition)
+dds <- DESeqDataSetFromMatrix(DEgenecount, coldata, design = ~condition)
+dds <- DESeq(dds) 
+vsd<-varianceStabilizingTransformation(dds,blind = FALSE)
+#这里是用DEseq的标准化方法，也可以直接用fpkm
+vstmat<-assay(vsd)
+pca<-PCA(t(vstmat),scale.unit = FALSE,ncp = 5,graph = FALSE)
+group<-read.csv("group.CSV")
+fviz_pca_ind(pca, geom.ind  = c("point","text"),
+            col.ind = group$GROUP, # color by groups
+            pointsize = 1.5, pointshape = 16,
+            addEllipses = T, 
+            repel = F ,
+            legend.title = "Tissue"
+)
+
+
+
