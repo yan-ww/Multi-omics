1fb772166a6516c96c15c06dce6faac74ad1e9ac
yanny <77775253+yan-ww@users.noreply.github.com>
2022-05-10 20:15:45 +0800

    Create heatmap.R

---
+++ heatmap.R
@@ -0,0 +1,27 @@
+setwd("E:/BIO/EP/data/")
+library(pheatmap)
+
+
+#在做热图前最重要的一步是标准化
+#方法一：DEseq标准化方法
+library(DESeq2)
+DEgenecount<-read.csv("DE")
+rownames(DEgenecount)<-DEgenecount[,1]
+DEgenecount<-DEgenecount[,-1]
+group<-read.csv("group.CSV")
+condition<-group$GROUP
+coldata<-data.frame(row.names=colnames(DEgenecount),condition)
+dds <- DESeqDataSetFromMatrix(DEgenecount, coldata, design = ~condition)
+dds <- DESeq(dds) 
+vsd<-varianceStabilizingTransformation(dds,blind = FALSE)
+vstmat<-assay(vsd)
+pheatmap(vstmat,show_rownames = FALSE,scale = "row")#画图
+
+
+方法二：vegen包的标准化函数
+library(vegan)
+d<-decostand(DE,"standardize",MARGIN = 1)#画图
+pheatmap(vstmat,show_rownames = FALSE,show_colnames = FALSE)
+
+dev.off()
+rm(list = ls())
