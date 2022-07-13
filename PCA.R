eaf57eafb054e34b83e04479f04fa67485853b6b
yanny <77775253+yan-ww@users.noreply.github.com>
2022-07-13 10:33:46 +0800

    Update PCA.R

--- PCA.R
+++ PCA.R
@@ -1,4 +1,4 @@
-setwd("E:/BIO/EP/data/")
+setwd("E:/")
 library(DESeq2)
 library(factoextra)
 library(FactoMineR)
@@ -7,24 +7,24 @@ library(vegan)
 count<-read.csv("DEgenecount.csv",header=T,row.names=1)
 datacount<-count
 summary(is.numeric(datacount[,1]))
-group<-read.csv("group.CSV")
-condition<-group$GROUP
-coldata<-data.frame(row.names=colnames(datacount),condition)
-dds <- DESeqDataSetFromMatrix(count, datacoldata, design = ~condition)
+b<-colnames(datacount)
+condition<-substring(b,nchar(b))
+people<-substr(b,3,nchar(b)-1)
+coldata<-data.frame(row.names=colnames(datacount),condition,people)
+dds <- DESeqDataSetFromMatrix(datacount, coldata, design = ~people+condition)
 dds <- DESeq(dds) 
 vsd<-varianceStabilizingTransformation(dds,blind = FALSE)
 #这里是用DEseq的标准化方法，也可以直接用fpkm
 vstmat<-assay(vsd)
 pca<-PCA(t(vstmat),scale.unit = FALSE,ncp = 5,graph = FALSE)
-group<-read.csv("group.CSV")
 fviz_pca_ind(pca, geom.ind  = c("point","text"),
-            col.ind = group$GROUP, # color by groups
+            col.ind = condition, # color by groups
             pointsize = 1.5, pointshape = 16,
             addEllipses = T, 
             repel = F ,
             legend.title = "Tissue",
-            title="youth vs adults"
-)+theme(plot.title = element_text(hjust = 0.5))
+            title="FvsN")+
+theme(plot.title = element_text(hjust = 0.5))
 
 
 
